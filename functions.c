cdbf98ab9b598f8f8a273b8a541ad8c2b9ca7ff2
catilus <helene.m.piet@gmail.com>
2025-04-02 19:18:11 -0700

    Modified readWord() to reveal duplicate letters all at once.

--- functions.c
+++ functions.c
@@ -9,9 +9,10 @@ char getCharacter() //Ask and retrieve character user input
 	printf("Enter a letter: ");
 		
 	char character = 0;
-	character = getchar(); //gets the first character entered
-	character = toupper(character); //uppercase character
-	while (getchar() != '\n'); //prevents multiple characters from being stored in character at once
+	character = getchar();	//gets the first character entered
+	character = toupper(character);	//uppercase character
+	
+	while (getchar() != '\n') {};	//consumes leftover characters in the input buffer
 
 	return character;
 }
@@ -19,27 +20,28 @@ char getCharacter() //Ask and retrieve character user input
 void readWord(char secretWord[], char userWord[])
 {
 	char character = 0;
-	int counter = 0;
+	int counter = 1;
 	
+	// Compares mystery word to input word and keeps looping around until either secret word is found or counter reaches 10 tries
 	while (strcmp(secretWord, userWord) != 0 && counter < 11)
 	{		
-		character = getCharacter();
-		char *ptr = strchr(secretWord, character);
-		counter++;
+		character = getCharacter(); // Gets a letter from user
+		char *ptr = secretWord;	// Finds first occurence of letter in secret word
 		
-		if (ptr != NULL)
+		while ((ptr = strchr(ptr, character)) != NULL)	// If inputted letter is in secret word
 		{		
-			int index = ptr - secretWord;
-			userWord[index] = character;
-			printf("%s\n", userWord);
+			int index = ptr - secretWord; // Calculate array index of letter in secret word
+			userWord[index] = character;	// Reveal letter in user word at calculated index
+			ptr++; // Move to next character to avoid infinite loop
 		}
-		else
-		{
-			printf("%s\n", userWord);
-		}
+		
+		printf("%s\n", userWord);	// Show updated word
+		
+		counter++;	// Increments counter of tries
 	}
 	
-	if (counter < 11)
+	// Once either secret word is found or number of tries = 10, print associated message
+	if (counter < 10)
 	{
 		printf("\nYou found the mystery word in %d tries.\n", counter);
 	}
--- main.c
+++ main.c
@@ -6,21 +6,29 @@
 
 int main(int argc, char *argv[])
 {
-	char secretWord[] = "OLIVE";
-	size_t size = sizeof(secretWord) / sizeof(secretWord[0]);
-	char *userWord = malloc(size);
+	char secretWord[] = "HELENE";
 	
+	// Initialize displayed word based on size of secret word
+	size_t size = sizeof(secretWord) / sizeof(secretWord[0]);
+	char *userWord = (char *)malloc(size);
+	if (userWord == NULL)
+	{
+		exit(0);
+	}
 	
 	for (int i=0; i < (size-1); i++)
 	{
 		userWord[i] = '*';	
 	}
 	
+	// Main screen instructions 
 	printf("\nWelcome to Hangman!\n\n");
 	
 	readWord(secretWord, userWord);
 	
+	// Freeing dynamically allocated memory
 	free(userWord);
+	userWord = NULL;
 	
 	return 0;
 }
