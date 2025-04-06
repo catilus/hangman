e7b9b2c880589af7b3c3cadfde15fb26977626f2
catilus <helene.m.piet@gmail.com>
2025-04-05 19:05:48 -0700

    Implemented counter of life and winning/losing conditions. Improved UI.

--- functions.c
+++ functions.c
@@ -10,9 +10,9 @@ char *chooseRandomWord()
 	char *listOfWords[] = {"ARACHNEA", "BEAR", "COYOTE", "DOLPHIN", "ELEPHANT", "FOX",
 						"GIRAFFE", "HIPPOPOTAMUS", "IBIS", "JAGUARANDI", "KOALA", "LION", "MOOSE",
 						"NAUTILUS", "OSTRICH", "PENGUIN", "QUAIL", "ROOSTER", "SNAKE", "TAMARIND",
-						"URCHIN" "VOLLE", "WAPITI", "XENOPS", "YAX", "ZEBRA"};
-	size_t length = sizeof(listOfWords) / sizeof(listOfWords[0]) + 1;		
-	printf("%zu", length);			
+						"URCHIN" "VOLLE", "WAPITI", "XENOPS", "YAK", "ZEBRA"};
+	
+	size_t length = sizeof(listOfWords) / sizeof(listOfWords[0]) + 1;				
 	int randomNumber = rand() % length + 1;
 	
 	return listOfWords[randomNumber];
@@ -59,16 +59,21 @@ char getCharacter()
 	return toupper(character);
 }
 
+
 // Function checks if input letter is in secretWord
 void readWord(char secretWord[], char userWord[])
 {	
 	// Calculate length of secret word
 	size_t length = strlen(secretWord);
+	
+	// Initialize counter of user tries
+	int counter = 0;
 		
 	// Keep prompting for characters until secretWord is found
-	while (strcmp(secretWord, userWord) != 0)
+	while (strcmp(secretWord, userWord) != 0 && counter < 10)
 	{
 		char character = getCharacter();
+		int letterFound = 0;
 		 
 		// Iterates over characters in word
 		for (int i=0; i < length; i++)
@@ -77,14 +82,33 @@ void readWord(char secretWord[], char userWord[])
 			if (character == secretWord[i])
 			{
 				userWord[i] = character;
+				letterFound = 1;
 			}
 			else
 			{
 				continue;
 			}
 		}
-		printf("%s\n\n", userWord);
+		
+		if (letterFound != 0)
+		{
+			printf("%s\n\n", userWord);
+		}
+		else
+		{
+			counter++;
+			printf("\n%c is not in the secret word. \nYou have %d tries left.\n", character, (10 - counter));
+			printf("________________\n\n");
+			printf("%s\n\n", userWord);
+		}	
 	}
 
-	printf("\nYou won!\n\n");
+	if (strcmp(secretWord, userWord) == 0)
+	{
+		printf("\nYOU WON!!!\n\n");
+	}
+	else
+	{
+		printf("The secret word was: %s\n", secretWord);
+	}
 }
